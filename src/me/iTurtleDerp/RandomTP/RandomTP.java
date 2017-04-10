f8ee9ced17174ea482c199c52de84c2b9e57a338
iTurtleDerp <thatoneturtlegamer@gmail.com>
2017-04-10 14:41:38 +0100

    New files - Version 1.2

--- src/me/iTurtleDerp/RandomTP/RandomTP.java
+++ src/me/iTurtleDerp/RandomTP/RandomTP.java
@@ -2,8 +2,10 @@ package me.iTurtleDerp.RandomTP;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,41 +14,57 @@ public class RandomTP extends JavaPlugin
 	@Override
 	public void onEnable()
 	{
-		getLogger().info("RandomTP Enabling");
+		getLogger().info("RandomTP Enabled");
 	}
 
 	@Override
 	public void onDisable()
 	{
-		getLogger().info("RandomTP Disabling");
+		getLogger().info("RandomTP Disabled");
 	}
 
 	public boolean onCommand(CommandSender theSender, Command cmd, String commandLabel, String[] args)
 	{
-		Player player = (Player) theSender;
-		
-		if(commandLabel.equalsIgnoreCase("randomtp"))
+		Server server = getServer();
+		ConsoleCommandSender console = server.getConsoleSender();
+		if(!(theSender instanceof Player))
 		{
-			if(player.hasPermission("randomtp.tp"))
-			{			
-			int randomX = (int) (Math.random() * 1000);
-			int randomZ = (int) (Math.random() * 1000);
-			int randomY = player.getWorld().getHighestBlockYAt(randomX, randomZ);
-			
-			Location randomLocation = new Location(player.getWorld(), randomX, randomY, randomZ);
-			player.teleport(randomLocation);
-			player.sendMessage(ChatColor.GOLD + "You were teleported to X: " + randomX + "Y: " + randomY + "Z: " + randomZ);
-			}
-			else player.sendMessage(ChatColor.RED + "You don't have permission to randomly teleport!");
-		}
+			console.sendMessage(ChatColor.RED + "You have to be a player to use this command!");
+		} 
+		if(theSender instanceof Player)
+		{	
+			Player player = (Player) theSender;
+			if(commandLabel.equalsIgnoreCase("randomtp"))
+			{
+				if(player.hasPermission("randomtp.tp"))
+				{			
+					int randomX = (int)(1000 + Math.random() * 1001);
+					int randomZ = (int)(1000 + Math.random() * 1001);
+					int randomY = player.getWorld().getHighestBlockYAt(randomX, randomZ);
 
+					Location randomLocation = new Location(player.getWorld(), randomX, randomY, randomZ);
+					player.teleport(randomLocation);
+					player.sendMessage(ChatColor.GOLD + "You were teleported to X: " + randomX + " Y: " + randomY + " Z: " + randomZ);
+				}
+				else player.sendMessage(ChatColor.RED + "You don't have permission to randomly teleport!");
+			} else {
+				if(commandLabel.equalsIgnoreCase("rtp"))
+				{
+					if(player.hasPermission("randomtp.tp"))
+					{			
+						int randomX = (int)(1000 + Math.random() * 1001);
+						int randomZ = (int)(1000 + Math.random() * 1001);
+						int randomY = player.getWorld().getHighestBlockYAt(randomX, randomZ);
+
+						Location randomLocation = new Location(player.getWorld(), randomX, randomY, randomZ);
+						player.teleport(randomLocation);
+						player.sendMessage(ChatColor.GOLD + "You were teleported to X: " + randomX + " Y: " + randomY + " Z: " + randomZ);
+					}
+					else player.sendMessage(ChatColor.RED + "You don't have permission to randomly teleport!");
+				}
+			}
+			return true;
+		}
 		return true;
 	}
-
-
-
-
-	//<   >   <=   =>   !=   ==   %   +   -   /   *
-
-	// || &&
 }
